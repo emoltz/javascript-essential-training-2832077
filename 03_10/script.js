2bf362c73d3ab0a5f84da8fdb9e2d4d196effbfa
emoltz <33405530+emoltz@users.noreply.github.com>
2021-06-25 16:02:43 -0400

    3-10 added class

--- 03_10/script.js
+++ 03_10/script.js
@@ -2,7 +2,38 @@
  * Create a class for the Backpack object type.
  * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
  */
-import Backpack from "./Backpack.js";
+
+class Backpack {
+  constructor(
+    // Defines parameters:
+    name,
+    volume,
+    color,
+    pocketNum,
+    strapLengthL,
+    strapLengthR,
+    lidOpen
+  ) {
+    // Define properties:
+    this.name = name;
+    this.volume = volume;
+    this.color = color;
+    this.pocketNum = pocketNum;
+    this.strapLength = {
+      left: strapLengthL,
+      right: strapLengthR,
+    };
+    this.lidOpen = lidOpen;
+  }
+  // Add methods like normal functions:
+  toggleLid(lidStatus) {
+    this.lidOpen = lidStatus;
+  }
+  newStrapLength(lengthLeft, lengthRight) {
+    this.strapLength.left = lengthLeft;
+    this.strapLength.right = lengthRight;
+  }
+}
 
 const everydayPack = new Backpack(
   "Everyday Backpack",
